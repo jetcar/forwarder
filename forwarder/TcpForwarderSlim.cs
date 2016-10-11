f3cf739eb16e131b0cbd8c4584d076def0da47de
vladimir.kruglov <vladimir.kruglov@arvato.com>
2016-10-11 16:17:44 +0300

    send same request an all adapters

--- forwarder/TcpForwarderSlim.cs
+++ forwarder/TcpForwarderSlim.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -14,20 +15,32 @@ namespace forwarder
             _mainSocket.Bind(local);
             _mainSocket.Listen(10);
 
+            var localSockets = new List<Socket>();
+            foreach (var ipAddress in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (ipAddress.ToString().Contains("10.94.136.231"))
+                    continue;
+                if (ipAddress.ToString().Contains(":"))
+                    continue;
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Bind(new IPEndPoint(ipAddress, 8091));
+                socket.Listen(10);
+                localSockets.Add(socket);
+            }
             while (true)
             {
                 var source = _mainSocket.Accept();
                 var destination = new TcpForwarderSlim();
-                var state = new State(source, destination._mainSocket);
+                var state = new State(source, destination._mainSocket, localSockets);
                 destination._mainSocket.Bind(new IPEndPoint(IPAddress.Parse("10.94.136.231"), 0));
-                destination.Connect(remote, source);
+                destination.Connect(remote, source, localSockets);
                 source.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
             }
         }
 
-        private void Connect(EndPoint remoteEndpoint, Socket destination)
+        private void Connect(EndPoint remoteEndpoint, Socket destination, List<Socket> local)
         {
-            var state = new State(_mainSocket, destination);
+            var state = new State(_mainSocket, destination, local);
             _mainSocket.Connect(remoteEndpoint);
             _mainSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, OnDataReceive, state);
         }
@@ -40,16 +53,32 @@ namespace forwarder
                 var bytesRead = state.SourceSocket.EndReceive(result);
                 if (bytesRead > 0)
                 {
-                    //var str = Encoding.UTF8.GetString(state.Buffer, 0, bytesRead);
-                    //Console.WriteLine(str);
+                    Console.WriteLine("from:" + state.SourceSocket.LocalEndPoint + " to " +
+                                      state.DestinationSocket.LocalEndPoint);
                     state.DestinationSocket.Send(state.Buffer, bytesRead, SocketFlags.None);
+                    try
+                    {
+                        foreach (var socket in state.Local)
+                        {
+                            socket.Send(state.Buffer, bytesRead, SocketFlags.None);
+                            socket.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                    }
+
                     state.SourceSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
                 }
             }
-            catch
+            catch (Exception Ex)
             {
                 state.DestinationSocket.Close();
                 state.SourceSocket.Close();
+                foreach (var socket in state.Local)
+                {
+                    socket.Close();
+                }
             }
         }
 
@@ -57,12 +86,14 @@ namespace forwarder
         {
             public Socket SourceSocket { get; private set; }
             public Socket DestinationSocket { get; private set; }
+            public List<Socket> Local { get; private set; }
             public byte[] Buffer { get; private set; }
 
-            public State(Socket source, Socket destination)
+            public State(Socket source, Socket destination, List<Socket> local)
             {
                 SourceSocket = source;
                 DestinationSocket = destination;
+                Local = local;
                 Buffer = new byte[65536];
             }
         }
