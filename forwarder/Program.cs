3704a7bff3adc62c4d3459e63c4e243bde4bc8da
vladimir.kruglov <vladimir.kruglov@arvato.com>
2016-10-11 15:30:46 +0300

    forwarder first commit

---
+++ forwarder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forwarder
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TcpForwarderSlim forwarderSlim = new TcpForwarderSlim();
+            forwarderSlim.Start(new IPEndPoint(IPAddress.Any, 8090), new IPEndPoint(IPAddress.Parse("10.30.138.135"), 8090));
+            Console.ReadLine();
+        }
+    }
+}
