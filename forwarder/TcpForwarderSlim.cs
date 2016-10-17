08b65ccf0a6c7a79055a7f3d68180c0dae7528b1
vladimir.kruglov <vladimir.kruglov@arvato.com>
2016-10-17 13:34:59 +0300

    receive async try

--- forwarder/TcpForwarderSlim.cs
+++ forwarder/TcpForwarderSlim.cs
@@ -11,7 +11,6 @@ namespace forwarder
     public class TcpForwarderSlim
     {
         private readonly Socket _mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        public static List<State> Sockets = new List<State>();
 
         public static Action SyncSocket;
         public static Action<IntPtr, string> UpdateRequest;
@@ -32,35 +31,194 @@ namespace forwarder
             }
             while (true)
             {
-                var source = _mainSocket.Accept();
-                var destination = new TcpForwarderSlim();
-                if (SyncSocket != null)
-                    SyncSocket.Invoke();
-                var state = new State(source, destination._mainSocket, localSockets, null);
-                Sockets.Add(state);
+                try
+                {
+                    var source = _mainSocket.Accept();
+                    var destination = new TcpForwarderSlim();
+                    if (SyncSocket != null)
+                        SyncSocket.Invoke();
+                    var state = new State(source, destination._mainSocket, localSockets, null);
 
-                destination._mainSocket.Bind(new IPEndPoint(IPAddress.Parse("10.94.136.231"), 0));
-                destination.Connect(remote, source, localSockets, state);
-                source.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
+                    destination._mainSocket.Bind(new IPEndPoint(IPAddress.Parse("10.94.136.231"), 0));
+                    destination.Connect(remote, source, localSockets, state);
+
+                    SocketAsyncEventArgs readsocket = new SocketAsyncEventArgs();
+                    readsocket.Completed += new EventHandler<SocketAsyncEventArgs>(ReadLocal);
+                    readsocket.SetBuffer(state.Buffer, 0, state.Buffer.Length);
+                    readsocket.UserToken = state;
+                    source.ReceiveAsync(readsocket);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
 
         private void Connect(EndPoint remoteEndpoint, Socket destination, List<IPEndPoint> local, State destinationstate)
         {
             var state = new State(_mainSocket, destination, local, destinationstate);
-            Sockets.Add(state);
             _mainSocket.Connect(remoteEndpoint);
-            _mainSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, OnDataReceiveSendBack, state);
+
+            SocketAsyncEventArgs readsocket = new SocketAsyncEventArgs();
+            readsocket.Completed += new EventHandler<SocketAsyncEventArgs>(ReadRemote);
+            readsocket.SetBuffer(state.Buffer, 0, state.Buffer.Length);
+            readsocket.UserToken = state;
+            _mainSocket.ReceiveAsync(readsocket);
+        }
+
+        private void ReadLocal(object sender, SocketAsyncEventArgs readSocket)
+        {
+            State state = readSocket.UserToken as State;
+
+            if (readSocket.BytesTransferred > 0)
+            {
+                try
+                {
+                    //SocketError.Success indicates that the last operation on the underlying socket succeeded
+                    if (readSocket.SocketError == SocketError.Success)
+                    {
+                        int bytesRead = readSocket.BytesTransferred;
+
+                        string responce = Encoding.UTF8.GetString(state.Buffer, 0, bytesRead);
+
+                        if (responce.Contains("HTTP/1.1 403")
+                            ||
+                            responce.Contains("HTTP/1.1 502")
+                            )
+                        {
+                            state.SourceSocket.Close();
+                            Console.WriteLine("HTTP/1.1 403");
+                        }
+
+                        var status = "";
+                        if (responce.StartsWith("HTTP"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (responce.StartsWith("GET"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (responce.StartsWith("POST"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (responce.StartsWith("CONNECT"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (UpdateRequest != null)
+                            UpdateRequest.Invoke(state.SourceSocket.Handle, status);
+
+                        Console.WriteLine("from:" + state.SourceSocket.LocalEndPoint + " to " +
+                                          state.DestinationSocket.LocalEndPoint + " " + status);
+
+                        state.ByteRequest = state.Buffer;
+                        state.BytesRead = bytesRead;
+                        state.StringRequest = responce;
+
+                        state.DestinationSocket.Send(state.Buffer, bytesRead, SocketFlags.None);
+                        var io = state.SourceSocket.ReceiveAsync(readSocket);
+                    }
+                    else
+                    {
+                        //state.SourceSocket.Close();
+                    }
+                }
+                catch (Exception)
+                {
+                    state.SourceSocket.Close();
+                }
+            }
+            else
+            {
+                //state.SourceSocket.Close();
+            }
+        }
+
+        private void ReadRemote(object sender, SocketAsyncEventArgs readSocket)
+        {
+            State state = readSocket.UserToken as State;
+
+            if (readSocket.BytesTransferred > 0)
+            {
+                try
+                {
+                    //SocketError.Success indicates that the last operation on the underlying socket succeeded
+                    if (readSocket.SocketError == SocketError.Success)
+                    {
+                        int bytesRead = readSocket.BytesTransferred;
+
+                        string responce = Encoding.UTF8.GetString(state.Buffer, 0, bytesRead);
+
+                        if (responce.Contains("HTTP/1.1 403")
+                            ||
+                            responce.Contains("HTTP/1.1 502")
+                        )
+                        {
+                            state.SourceSocket.Close();
+                            Console.WriteLine("HTTP/1.1 403");
+                        }
+
+                        var status = "";
+                        if (responce.StartsWith("HTTP"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (responce.StartsWith("GET"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (responce.StartsWith("POST"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (responce.StartsWith("CONNECT"))
+                        {
+                            status = responce.Split('\r')[0];
+                        }
+                        if (UpdateRequest != null)
+                            UpdateRequest.Invoke(state.SourceSocket.Handle, status);
+
+                        state.ByteRequest = state.Buffer;
+                        state.BytesRead = bytesRead;
+                        state.StringRequest = responce;
+
+                        state.DestinationSocket.Send(state.Buffer, bytesRead, SocketFlags.None);
+
+                        Console.WriteLine("from:" + state.SourceSocket.LocalEndPoint + " to " +
+                                          state.DestinationSocket.LocalEndPoint + " " + status);
+
+                        var io = state.SourceSocket.ReceiveAsync(readSocket);
+                    }
+                    else
+                    {
+                        state.SourceSocket.Close();
+                        state.DestinationSocket.Close();
+                    }
+                }
+                catch (Exception)
+                {
+                    state.SourceSocket.Close();
+                    state.DestinationSocket.Close();
+                }
+            }
+            else
+            {
+                state.SourceSocket.Close();
+                state.DestinationSocket.Close();
+            }
         }
 
         private static void OnDataReceiveSendBack(IAsyncResult result)
         {
-            Console.WriteLine("Sockets:" + Sockets.Count);
             var state = (State)result.AsyncState;
             try
             {
                 int bytesRead;
-                bytesRead = state.SourceSocket.EndReceive(result);
+                SocketError err;
+                bytesRead = state.SourceSocket.EndReceive(result, out err);
 
                 if (bytesRead > 0)
                 {
@@ -86,19 +244,19 @@ namespace forwarder
                                         var lines = state.StringRequest.Split('\r');
                                         foreach (var line in lines)
                                         {
-                                            if (line.Contains("Host:"))
-                                            {
-                                                socket.Connect(new DnsEndPoint(line.Replace("\nHost:", "").Replace(" ", ""), 80));
+                                            //if (line.Contains("Host:"))
+                                            //{
+                                            //    socket.Connect(new DnsEndPoint(line.Replace("\nHost:", "").Replace(" ", ""), 80));
 
-                                                if (socket.Connected)
-                                                {
-                                                    var innerState = new State(socket, state.DestinationSocket, new List<IPEndPoint>(), state);
-                                                    socket.Send(state.ByteRequest, state.BytesRead, SocketFlags.None);
-                                                    socket.BeginReceive(innerState.Buffer, 0, innerState.Buffer.Length, 0, OnDataReceive,
-                                                        innerState);
-                                                }
-                                                break;
-                                            }
+                                            //    if (socket.Connected)
+                                            //    {
+                                            //        var innerState = new State(socket, state.DestinationSocket, new List<IPEndPoint>(), state);
+                                            //        socket.Send(state.ByteRequest, state.BytesRead, SocketFlags.None);
+                                            //        socket.BeginReceive(innerState.Buffer, 0, innerState.Buffer.Length, 0, OnDataReceive,
+                                            //            innerState);
+                                            //    }
+                                            //    break;
+                                            //}
                                             //if (line.Contains("CONNECT")) //not really needed now
                                             //{
                                             //    var url = line.Replace("CONNECT", "").Replace(" ", "").Replace("HTTP/1.1", "");
@@ -157,16 +315,14 @@ namespace forwarder
 
                     Console.WriteLine("from:" + state.SourceSocket.LocalEndPoint + " to " +
                                       state.DestinationSocket.LocalEndPoint + " " + status);
+                    //state.DestinationSocket.BeginSend(state.Buffer, 0, bytesRead, SocketFlags.None, BeginSend, state);
                     state.DestinationSocket.Send(state.Buffer, bytesRead, SocketFlags.None);
-
                     state.ByteRequest = state.Buffer;
                     state.BytesRead = bytesRead;
                     state.StringRequest = responce;
 
-                    if (state.SourceSocket.Connected && state.DestinationSocket.Connected)
-                        state.SourceSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceiveSendBack, state);
-                    else
-                        Sockets.Remove(state);
+                    state.SourceSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceiveSendBack,
+                        state);
                 }
             }
             catch (Exception Ex)
@@ -175,81 +331,6 @@ namespace forwarder
 
                 state.DestinationSocket.Close();
                 state.SourceSocket.Close();
-                Sockets.Remove(state);
-                if (SyncSocket != null)
-                    SyncSocket.Invoke();
-
-                //foreach (var socket in state.Local)
-                //{
-                //    socket.Close();
-                //}
-            }
-        }
-
-        private static void OnDataReceive(IAsyncResult result)
-        {
-            Console.WriteLine("Sockets:" + Sockets.Count);
-            var state = (State)result.AsyncState;
-            try
-            {
-                int bytesRead;
-                bytesRead = state.SourceSocket.EndReceive(result);
-
-                if (bytesRead > 0)
-                {
-                    string responce = Encoding.UTF8.GetString(state.Buffer, 0, bytesRead);
-
-                    if (responce.Contains("HTTP/1.1 403")
-                        ||
-                        responce.Contains("HTTP/1.1 502")
-                        )
-                    {
-                        state.SourceSocket.Close();
-                        Console.WriteLine("HTTP/1.1 403");
-                    }
-
-                    var status = "";
-                    if (responce.StartsWith("HTTP"))
-                    {
-                        status = responce.Split('\r')[0];
-                    }
-                    if (responce.StartsWith("GET"))
-                    {
-                        status = responce.Split('\r')[0];
-                    }
-                    if (responce.StartsWith("POST"))
-                    {
-                        status = responce.Split('\r')[0];
-                    }
-                    if (responce.StartsWith("CONNECT"))
-                    {
-                        status = responce.Split('\r')[0];
-                    }
-                    if (UpdateRequest != null)
-                        UpdateRequest.Invoke(state.SourceSocket.Handle, status);
-
-                    Console.WriteLine("from:" + state.SourceSocket.LocalEndPoint + " to " +
-                                      state.DestinationSocket.LocalEndPoint + " " + status);
-                    state.DestinationSocket.Send(state.Buffer, bytesRead, SocketFlags.None);
-
-                    state.ByteRequest = state.Buffer;
-                    state.BytesRead = bytesRead;
-                    state.StringRequest = responce;
-                    //if (responce.EndsWith("\r\n\r\n") && state.SourceSocket.Available == 0)
-                    //{
-                    //    state.SourceSocket.Close();
-                    //}
-                    //else
-                    state.SourceSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
-                }
-            }
-            catch (Exception Ex)
-            {
-                Console.WriteLine(Ex.Message);
-
-                state.DestinationSocket.Close();
-                state.SourceSocket.Close();
-                Sockets.Remove(state);
                 if (SyncSocket != null)
                     SyncSocket.Invoke();
 
@@ -270,12 +351,15 @@ namespace forwarder
             public string StringRequest { get; set; }
             public int BytesRead { get; set; }
 
+            public State DestinationState { get; set; }
+
             public State(Socket source, Socket destination, List<IPEndPoint> local, State state)
             {
                 SourceSocket = source;
                 DestinationSocket = destination;
                 Local = local;
                 Buffer = new byte[65536];
+                DestinationState = state;
             }
         }
     }
